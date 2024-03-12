f62808a65fb8c4be65bcdef9deec6f08a4e7c9d3
prashantsingh15 <prashantsingh15092002@gmail.com>
2024-03-12 08:22:06 +0530

    commit

--- Question3.js
+++ Question3.js
@@ -1,2 +1,8 @@
-// How to Empty an array in Javascript ?
+// How to Empty an array in Javascript ? Do not reset it to a new array , and do not loop through to pop each value ?
+
+
+var arr = [1,2,3,4,5,6,7]
+arr.length = 0
+console.log(arr)
+
 
